f69ccb776ec213f73ea62c5f8f6c52dd2ddd0d69
Beatriz Ferreira <biatizferreira04@gmail.com>
2024-05-19 18:35:44 +0100

    Other heuristic - nearest neighbor

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -4,6 +4,7 @@
 
 #include "Menu.h"
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -39,6 +40,7 @@ void Menu::mainMenu() {
                 break;
             case 3:
                 // Other Heuristics
+                choice3();
                 break;
             case 4:
                 // TSP in the Real World
@@ -86,6 +88,16 @@ void Menu::choice1() {
     cout << "Time: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl << endl;
 }
 
+void Menu::choice3() {
+    auto start = std::chrono::high_resolution_clock::now();
+    double totalDistance = nearestNeighborTSP(this->g);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "Total Distance: " << totalDistance << std::endl;
+    std::cout << "Execution Time: " << duration.count() << " seconds\n";
+}
+
 void Menu::TSPBacktracking(int curr, vector<int>& path, vector<int>& bestPath, double& bestDist, double currDist) {
     auto currVertex = g->findVertex(curr);
     currVertex->setVisited(true);
@@ -114,3 +126,82 @@ void Menu::TSPBacktracking(int curr, vector<int>& path, vector<int>& bestPath, d
     }
 }
 
+double Menu::nearestNeighborTSP(Graph<int>* graph) {
+    double totalDistance = 0.0;
+    std::vector<bool> visited(graph->getNumVertex(), false);
+    int current = 0;
+    visited[current] = true;
+    bool isreal = false;
+    bool found = false;
+    if(graph->getVertexSet().front()->getLongitude() != 0){
+        isreal = true;
+    }
+
+    for (size_t i = 1; i < graph->getNumVertex(); ++i) {
+        double minDistance;
+        int next = nearestNeighbor(graph, visited, current, minDistance, isreal);
+        if (next == -1) break;
+        totalDistance += minDistance;
+        visited[next] = true;
+        current = next;
+    }
+
+    for(auto e: graph->findVertex(current)->getAdj()){
+        if(e->getDest() == graph->findVertex(0)){
+            totalDistance += e->getWeight();
+            found = true;
+            break;
+        }
+    }
+
+    // Return to the starting node
+    if(isreal && !found){
+        totalDistance += haversineDistance(*graph->findVertex(current), *graph->findVertex(0));
+    }
+
+    return totalDistance;
+}
+
+int Menu::nearestNeighbor(const Graph<int>* graph, const std::vector<bool>& visited, int current, double& minDistance, bool isreal) {
+    minDistance = std::numeric_limits<double>::max();
+    int nearest = -1;
+    bool found = false;
+    Vertex<int>* currentVertex = graph->findVertex(current);
+
+    for (Vertex<int>* vertex : graph->getVertexSet()) {
+        if (!visited[vertex->getInfo()]) {
+            double distance;
+            for(auto e : currentVertex->getAdj()){
+                if(e->getDest() == vertex){
+                    distance = e->getWeight();
+                    found = true;
+                }
+            }
+            if(isreal && !found){
+                distance = haversineDistance(*currentVertex, *vertex);
+                found = false;
+            }
+
+            if (distance < minDistance) {
+                minDistance = distance;
+                nearest = vertex->getInfo();
+            }
+        }
+    }
+    return nearest;
+}
+
+double Menu::haversineDistance(const Vertex<int>& node1, const Vertex<int>& node2) {
+    std::cout << node1.getLongitude() << std::endl;
+    const double R = 6371; // Earth radius in kilometers
+    double lat1 = node1.getLatitude() * M_PI / 180.0;
+    double lon1 = node1.getLongitude() * M_PI / 180.0;
+    double lat2 = node2.getLatitude() * M_PI / 180.0;
+    double lon2 = node2.getLongitude() * M_PI / 180.0;
+    double dlat = lat2 - lat1;
+    double dlon = lon2 - lon1;
+    double a = sin(dlat / 2) * sin(dlat / 2) +
+               cos(lat1) * cos(lat2) * sin(dlon / 2) * sin(dlon / 2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    return R * c;
+}
--- src/Menu.h
+++ src/Menu.h
@@ -17,8 +17,12 @@ public:
     Menu(Graph<int>* g);
     void mainMenu();
     void choice1();
+    void choice3();
 
     void TSPBacktracking(int curr, std::vector<int>& path, std::vector<int>& bestPath, double& bestDist, double currDist);
+    static double nearestNeighborTSP(Graph<int>* graph);
+    static int nearestNeighbor(const Graph<int>* graph, const std::vector<bool>& visited, int current, double& minDistance, bool isreal);
+    static double haversineDistance(const Vertex<int>& node1, const Vertex<int>& node2);
 };
 
 
